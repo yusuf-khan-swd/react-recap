b1d183f7f97fd2ce2f91568849e13749116b8df7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-18 22:06:18 +0600

    Add tailwind to vite project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
         error asperiores. Natus quas aliquid, rem quod nobis eveniet sed!
