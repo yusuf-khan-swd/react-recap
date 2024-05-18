73a13b71e61e779c9460a7174e366aa574feb988
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-18 23:37:26 +0600

    Add login and about page route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import About from "../pages/About";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
       },
     ],
   },
