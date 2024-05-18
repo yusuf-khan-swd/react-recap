8bfbd647bfaa10236cc7011f31b0a696d86f2284
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-18 23:51:46 +0600

    use container and center align the outlet elements

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,7 +6,9 @@ const MainLayout = () => {
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <div className="container mx-auto">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
