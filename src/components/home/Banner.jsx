d3062f2ac81c414836cf74515173e70d05c0e58b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-18 22:45:03 +0600

    Add navbar and banner from Daisy ui component

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,7 +1,17 @@
 const Banner = () => {
   return (
-    <div className="bg-zinc-400 min-h-[150px] flex justify-center items-center">
-      <h3 className="text-2xl">This is Banner</h3>
+    <div className="hero bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 };
