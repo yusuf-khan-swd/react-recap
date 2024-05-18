4d4bb4949e67017e0ee11da6215bbdfff45eda39
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-19 00:02:00 +0600

    Add style home page button

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
     <div className="flex flex-col justify-center items-center min-h-screen">
       <h1 className="text-7xl font-extrabold mb-8">Error: {status || 404}</h1>
       <p className="lg:text-3xl">{error?.message}</p>
-      <button className="mt-8">
+      <button className="btn bg-red-300  mt-8">
         <Link to="/">HomePage</Link>
       </button>
     </div>
