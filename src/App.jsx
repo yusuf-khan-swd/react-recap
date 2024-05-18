46cc52c3d18d6575281152b7b29effb49add81a8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-18 21:59:11 +0600

    Add some content and add style to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+        error asperiores. Natus quas aliquid, rem quod nobis eveniet sed!
+      </p>
+    </div>
+  );
 }
 
 export default App;
