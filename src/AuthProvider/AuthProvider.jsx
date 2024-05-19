045dffa2f10df7b74ce2279a9e75025e642e78ec
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-19 23:08:04 +0600

    get currentUser from firebase state

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,10 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import { createContext } from "react";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -7,13 +12,28 @@ export const AuthContext = createContext(null);
 const auth = getAuth(app);
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
   const googleProvider = new GoogleAuthProvider();
 
   const googleLogin = () => {
     return signInWithPopup(auth, googleProvider);
   };
 
-  const authInfo = { googleLogin };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        console.log(currentUser);
+      }
+    });
+
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = { user, googleLogin };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
