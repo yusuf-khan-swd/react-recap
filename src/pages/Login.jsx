687ebc31204c61c4f4b7e0cacf5f9b3b73e5b7f1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-19 21:55:05 +0600

    Update Login Form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,53 +1,77 @@
+import { Link } from "react-router-dom";
+import GoogleLogin from "../components/Login-Registration/GoogleLogin";
+
 const Login = () => {
+  const handleSUbmit = async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(email, password);
+  };
+
   return (
-    <div className="max-w-[840px] mx-auto">
-      <div className="hero min-h-[80vh] bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
-          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                  required
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-            </form>
+    <form onSubmit={handleSUbmit} className="hero min-h-[80vh] bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse max-w-[1040px] mx-auto">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                name="email"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                name="password"
+                required
+              />
+            </div>
+
+            <div className="form-control mt-6">
+              <input
+                className="btn bg-red-500 text-white"
+                type="submit"
+                value="Login"
+              />
+            </div>
+            <div className="mt-6">
+              <GoogleLogin />
+            </div>
+            <div className="mt-6">
+              <p>
+                New here?{" "}
+                <Link to="/register" className="text-red-500">
+                  Register
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
