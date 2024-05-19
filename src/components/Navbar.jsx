471c2301a1460577e6b6a71ec7c88f3092b519bd
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-19 22:04:18 +0600

    Add register route to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,9 @@ const Navbar = () => {
         <Link to="/login">Login</Link>
       </li>
       <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
         <Link to="/dashboard">Dashboard</Link>
       </li>
     </>
