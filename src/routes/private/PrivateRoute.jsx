7f0c19896bbea8ef7825bd877db8374f5852c6e2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-20 00:07:30 +0600

    add location history from Private route

--- src/routes/private/PrivateRoute.jsx
+++ src/routes/private/PrivateRoute.jsx
@@ -1,16 +1,17 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import LoadingSpinner from "../../components/LoadingSpinner";
 import useAuth from "../../hooks/useAuth";
 
 // eslint-disable-next-line react/prop-types
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
+  const location = useLocation();
 
   if (loading) return <LoadingSpinner />;
 
   if (user) return children;
 
-  return <Navigate to="/login" />;
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRoute;
