479f771b038cdb1a770d6ef013023a5d34a3981c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-19 22:03:27 +0600

    Update link use react-router-dom default Link component for links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   const menuItems = (
     <>
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/about">About</a>
+        <Link to="/about">About</Link>
       </li>
       <li>
-        <a href="/login">Login</a>
+        <Link to="/login">Login</Link>
       </li>
       <li>
-        <a href="/dashboard">Dashboard</a>
+        <Link to="/dashboard">Dashboard</Link>
       </li>
     </>
   );
