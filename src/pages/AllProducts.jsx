b867ee22c2923b5a9735f2dec69300610c11ad86
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-23 23:20:10 +0600

    use useEffect to get all shoes data from api

---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import SingleProduct from "../components/SingleProduct";
+
+const AllProducts = () => {
+  const [shoes, setShoes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/shoes")
+      .then((res) => res.json())
+      .then((data) => setShoes(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>All Products</h1>
+      <div className="flex justify-center gap-2">
+        {shoes?.slice(0, 3).map((shoe) => (
+          <SingleProduct key={shoe.id} shoe={shoe} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllProducts;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
 import MainLayout from "../layouts/MainLayout";
 import About from "../pages/About";
+import AllProducts from "../pages/AllProducts";
 import Dashboard from "../pages/Dashboard";
 import ErrorPage from "../pages/ErrorPage";
 import Home from "../pages/Home";
@@ -51,6 +52,14 @@ export const router = createBrowserRouter([
         element: (
           <PrivateRoute>
             <Dashboard />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "all-products",
+        element: (
+          <PrivateRoute>
+            <AllProducts />
           </PrivateRoute>
         ),
       },
