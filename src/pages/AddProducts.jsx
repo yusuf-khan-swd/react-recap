da406da99f36aa9f8cb7b4f7c7d1b5172939a838
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-23 23:42:17 +0600

    Add a form for AddProducts and get form value from the inputs

--- src/pages/AddProducts.jsx
+++ src/pages/AddProducts.jsx
@@ -1,7 +1,84 @@
 const AddProducts = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const id = form?.id.value;
+    const title = form?.title.value;
+    const brand = form?.brand.value;
+    const price = form?.price.value;
+    const description = form?.description.value;
+    const image_url = form?.image_url.value;
+
+    const data = { id, title, brand, price, description, image_url };
+
+    console.log(data);
+  };
+
   return (
     <div>
-      <h1>Add Products</h1>
+      <h1 className="text-5xl font-bold text-center">Add a Product</h1>
+
+      <div className="my-16">
+        <form onSubmit={handleSubmit}>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="title"
+              placeholder="Title"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="brand"
+              placeholder="Brand"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="number"
+              name="price"
+              placeholder="Price"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="description"
+              placeholder="Description"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="image_url"
+              placeholder="Image URL"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="id"
+              placeholder="ID"
+            />
+          </div>
+          <div className="mt-2 flex justify-center items-center">
+            <input
+              className="btn mt-4 w-full bg-red-500 text-white p-4"
+              type="submit"
+              value="Add product"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
