0988bdd540a128cc060b0e6ca470cab50f6e0d20
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-24 10:51:47 +0600

    Create SingleProductDashboard component

---
+++ src/components/dashboard/SingleProductDashboard.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom";
+
+const SingleProductDashboard = ({ shoe }) => {
+  const { id, title, brand, price, description } = shoe;
+
+  const handleDelete = () => {};
+
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <h3 className="text-xl font-semibold">Brand: {brand}</h3>
+        <h3 className="text-xl font-semibold">Price: ${price}</h3>
+        <p>Description: {description}</p>
+        <div className="card-actions justify-end">
+          <button className="btn bg-indigo-500 text-white">
+            <Link to={`/products/${id}`}>See details</Link>
+          </button>
+          <button className="btn bg-green-600 text-white">
+            <Link to={`edit/${id}`}>Edit</Link>
+          </button>
+          <button onClick={handleDelete} className="btn bg-red-500 text-white">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProductDashboard;
