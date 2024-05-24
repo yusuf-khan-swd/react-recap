5f31c4ed84a37d240bd10e477ae35bae501512a2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-24 11:12:25 +0600

    Rename shoes to product

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -2,23 +2,23 @@ import { useEffect, useState } from "react";
 import SingleProductDashboard from "../components/dashboard/SingleProductDashboard";
 
 const AllProducts = () => {
-  const [shoes, setShoes] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/shoes")
       .then((res) => res.json())
-      .then((data) => setShoes(data));
+      .then((data) => setProducts(data));
   }, []);
 
   const handleDeleteProduct = (id) => {
-    setShoes(shoes.filter((shoe) => shoe.id !== id));
+    setProducts(products.filter((product) => product.id !== id));
   };
 
   return (
     <div>
       <h1 className="text-5xl font-bold text-center">All Products</h1>
       <div className="my-16 flex justify-center flex-wrap gap-4">
-        {shoes.map((shoe) => (
+        {products.map((shoe) => (
           <SingleProductDashboard
             key={shoe.id}
             shoe={shoe}
