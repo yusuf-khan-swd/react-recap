24ea43a71f4a88ae21fcdc204012a15180eea72c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-24 11:32:44 +0600

    Use AddProduct page form for EditProduct form

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -4,7 +4,98 @@ const EditProduct = () => {
   const shoe = useLoaderData();
   console.log(shoe);
 
-  return <div>Edit Product</div>;
-};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    const form = e.target;
+
+    const id = form?.id.value;
+    const title = form?.title.value;
+    const brand = form?.brand.value;
+    const price = form?.price.value;
+    const description = form?.description.value;
+    const image_url = form?.image_url.value;
+
+    const data = { id, title, brand, price, description, image_url };
+
+    const res = await fetch("http://localhost:3000/shoes", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const resData = await res.json();
+    console.log(resData);
+
+    form.reset();
+  };
+
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-center">Add a Product</h1>
+
+      <div className="my-16">
+        <form onSubmit={handleSubmit}>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="title"
+              placeholder="Title"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="brand"
+              placeholder="Brand"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="number"
+              name="price"
+              placeholder="Price"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="description"
+              placeholder="Description"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="image_url"
+              placeholder="Image URL"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              className="bg-gray-100 p-4 w-full border border-black rounded-lg"
+              type="text"
+              name="id"
+              placeholder="ID"
+            />
+          </div>
+          <div className="mt-2 flex justify-center items-center">
+            <input
+              className="btn mt-4 w-full bg-red-500 text-white p-4"
+              type="submit"
+              value="Add product"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default EditProduct;
