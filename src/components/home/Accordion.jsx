67b1d0c76ddb6d090c59ce28fae5aea5fe23e552
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-26 11:51:50 +0600

    Add size chart with style for list and margin 20px for title

--- src/components/home/Accordion.jsx
+++ src/components/home/Accordion.jsx
@@ -9,13 +9,23 @@ const Accordion = () => {
             Ensure you get the perfect fit with our comprehensive size guide.
             Measure your feet accurately to find the right size for you.
           </p>
-          <h3 className="font-semibold my-2">How to Measure:</h3>
+          <p className="font-semibold my-5">How to Measure:</p>
           <ol className="list-decimal list-inside">
-            <li>Place your foot on a piece of paper.</li>
-            <li>Mark the end of your heel and the tip of your longest toe.</li>
-            <li>Measure the distance between the two marks.</li>
-            <li>Refer to our size chart to find your size.</li>
+            <li className="mb-2">Place your foot on a piece of paper.</li>
+            <li className="mb-2">
+              Mark the end of your heel and the tip of your longest toe.
+            </li>
+            <li className="mb-2">
+              Measure the distance between the two marks.
+            </li>
+            <li className="mb-2">Refer to our size chart to find your size.</li>
           </ol>
+          <p className="font-semibold my-5">Size Chart:</p>
+          <ul className="list-disc list-inside">
+            <li className="mb-2">US Sizes: 5, 6, 7, 8, 9, 10, 11, 12</li>
+            <li className="mb-2">UK Sizes: 4, 5, 6, 7, 8, 9, 10, 11</li>
+            <li className="mb-2">EU Sizes: 37, 38, 39, 40, 41, 42, 43, 44</li>
+          </ul>
         </div>
       </div>
       <div className="collapse collapse-arrow bg-base-200 mb-4">
