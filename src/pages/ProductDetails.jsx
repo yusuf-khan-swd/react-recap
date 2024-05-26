afe4cd9d887e369d1a820b4f6a3347f9846aa8b1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-26 10:46:16 +0600

    use card for product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -6,21 +6,22 @@ const ProductDetails = () => {
   const { brand, description, price, title } = shoe;
 
   return (
-    <div className="flex flex-col items-center">
-      <figure>
-        <img
-          className="h-[600px]"
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-        />
-      </figure>
-
-      <h1 className="text-5xl text-center font-bold">{title}</h1>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <h3 className="text-xl font-semibold">Brand: {brand}</h3>
-        <h3 className="text-xl font-semibold">Price: ${price}</h3>
-        <p>Description: {description}</p>
+    <div className="flex flex-col items-center my-16">
+      <div className="card bg-base-100 shadow-xl">
+        <figure>
+          <img
+            className="h-[600px]"
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <h1 className="text-5xl text-center font-bold my-2">{title}</h1>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <h3 className="text-xl font-semibold">Brand: {brand}</h3>
+          <h3 className="text-xl font-semibold">Price: ${price}</h3>
+          <p>Description: {description}</p>
+        </div>
       </div>
     </div>
   );
