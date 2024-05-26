8c14004d9d8708eb2431566914ca003b0d431b8c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-26 11:34:04 +0600

    Add Size Guide to accordion

--- src/components/home/Accordion.jsx
+++ src/components/home/Accordion.jsx
@@ -2,12 +2,20 @@ const Accordion = () => {
   return (
     <div className="mt-16">
       <div className="collapse collapse-arrow bg-base-200 mb-4">
-        <input type="radio" name="my-accordion-2" defaultChecked />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title text-xl font-medium">Size Guide</div>
         <div className="collapse-content">
-          <p>hello</p>
+          <p>
+            Ensure you get the perfect fit with our comprehensive size guide.
+            Measure your feet accurately to find the right size for you.
+          </p>
+          <h3 className="font-semibold my-2">How to Measure:</h3>
+          <ol className="list-decimal list-inside">
+            <li>Place your foot on a piece of paper.</li>
+            <li>Mark the end of your heel and the tip of your longest toe.</li>
+            <li>Measure the distance between the two marks.</li>
+            <li>Refer to our size chart to find your size.</li>
+          </ol>
         </div>
       </div>
       <div className="collapse collapse-arrow bg-base-200 mb-4">
