40ac44513fc15dadab5a3e3d6371bcac9f3c19e9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-26 11:12:52 +0600

    add border bottom for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 border-b-2">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
