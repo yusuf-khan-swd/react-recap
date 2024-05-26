90464e29f3e2e1880532719f14c48ecea48a63b2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-26 13:47:26 +0600

    update /dashboard route with path and layout link

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -6,7 +6,7 @@ const DashboardLayout = () => {
       <div className="col-span-2 bg-gray-300 min-h-screen p-12">
         <ul>
           <li className="border border-black p-4 text w-full">
-            <Link to={"home"}>
+            <Link to={"/dashboard"}>
               <button className="btn w-full">Dashboard</button>
             </Link>
           </li>
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -50,7 +50,7 @@ export const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "home",
+        path: "/dashboard",
         element: (
           <PrivateRoute>
             <Dashboard />
