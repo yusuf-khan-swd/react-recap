aa283c295ab74aebd7e1ca50530d2eba9a93e90f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-31 23:11:55 +0600

    add handleSubmit function with fetch api

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -4,6 +4,25 @@ const EditProfile = () => {
   const userInfo = useLoaderData();
   console.log(userInfo);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+
+    const user = { email, name };
+
+    fetch(`http://localhost:5000/user/profile/${userInfo?._id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div>
       <h1 className="text-3xl font-semibold text-center mb-4">
@@ -11,7 +30,7 @@ const EditProfile = () => {
       </h1>
       <div className="hero bg-base-200 p-8">
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form className="card-body" onSubmit={handleSubmit}>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
@@ -37,7 +56,9 @@ const EditProfile = () => {
               />
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-accent text-white">Update</button>
+              <button type="submit" className="btn btn-accent text-white">
+                Update
+              </button>
             </div>
           </form>
         </div>
